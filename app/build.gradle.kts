e3414b96345001f3a692725b4af678a8a4a19c9a
ondrejwretzl-dotcom <ondrej.wretzl@gmail.com>
2026-02-02 13:42:27 +0100

    Add lifecycle-service dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-service:2.6.2")
 }
